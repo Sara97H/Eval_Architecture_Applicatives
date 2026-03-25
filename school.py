6e33ca490285a65e1204d6c24e72854060b8bf91
Sara97H <sarahaddad.sh97@gmail.com>
2026-03-25 14:44:28 +0100

    Q10 : decorateur iterateur 4

--- school.py
+++ school.py
@@ -7,6 +7,13 @@ def add_matter_4(cls):
         self.matter_4 = matter_4
     cls.__init__ = new_init
     return cls
+
+def add_matter_4_iterator(cls):
+    def get_matter_4_iter(self):
+        return Matter4Iterator(self.students)
+    cls.get_matter_4_iter = get_matter_4_iter
+    return cls
+
 @add_matter_4
 class Student:
     def __init__(self, name, matter_1, matter_2, matter_3):
@@ -47,6 +54,18 @@ class Matter3Iterator(Iterator):
             return res
         raise StopIteration
     
+class Matter4Iterator(Iterator):
+    def __init__(self, students):
+        self.students = sorted(students, key=lambda s: s.matter_4, reverse=True)
+        self.index = 0
+    def __next__(self):
+        if self.index < len(self.students):
+            res = self.students[self.index]
+            self.index += 1
+            return res
+        raise StopIteration
+
+@add_matter_4_iterator
 class SchoolClass(Iterable):
      def __init__(self):
          self.students = []
@@ -72,17 +91,22 @@ class SchoolClass(Iterable):
 
 if __name__ == '__main__':
     school_class = SchoolClass()
-    school_class.add_student(Student('J', 10, 12, 13))
-    school_class.add_student(Student('A', 8, 2, 17))
-    school_class.add_student(Student('V', 9, 14, 14))
+    school_class.add_student(Student('J', 10, 12, 13,15))
+    school_class.add_student(Student('A', 8, 2, 17,10))
+    school_class.add_student(Student('V', 9, 14, 14, 12))
 
     # school_class.rank_matter_1()
     # school_class.rank_matter_2()
-    # school_class.rank_matter_3()
-
+    # school_class.rank_matter_3()*
+    
     for s in school_class:
         print(f"{s.name}: {s.matter_1}")
     for s in Matter2Iterator(school_class.students):
         print(f"{s.name}: {s.matter_2}")
     for s in Matter3Iterator(school_class.students):
         print(f"{s.name}: {s.matter_3}")
+    
+    for s in school_class.students:
+        print(f"{s.name}: {s.matter_4}")
+    for s in school_class.get_matter_4_iter():
+        print(f"{s.name}: {s.matter_4}")
