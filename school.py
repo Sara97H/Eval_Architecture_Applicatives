496825302dc5a70c6a5eaf63c0f184db7324823e
Sara97H <sarahaddad.sh97@gmail.com>
2026-03-25 13:33:34 +0100

    Q3 : methode rank_matter_1

--- school.py
+++ school.py
@@ -12,8 +12,14 @@ class SchoolClass:
     def add_student(self, student):
         self.students.append(student)
 
+def rank_matter_1(self):
+        self.students.sort(key=lambda s: s.matter_1, reverse=True)
+        for s in self.students:
+            print(f"{s.name}: {s.matter_1}")
+
 if __name__ == '__main__':
     school_class = SchoolClass()
     school_class.add_student(Student('J', 10, 12, 13))
     school_class.add_student(Student('A', 8, 2, 17))
     school_class.add_student(Student('V', 9, 14, 14))
+    school_class.rank_matter_1()
