7a6bbcad695bc27116e95c97ab512791abcdc60c
Sara97H <sarahaddad.sh97@gmail.com>
2026-03-25 13:37:53 +0100

    Q4 : methodes rank_matter_2 et 3

--- school.py
+++ school.py
@@ -16,10 +16,23 @@ def rank_matter_1(self):
         self.students.sort(key=lambda s: s.matter_1, reverse=True)
         for s in self.students:
             print(f"{s.name}: {s.matter_1}")
+        
+def rank_matter_2(self):
+        self.students.sort(key=lambda s: s.matter_2, reverse=True)
+        for s in self.students:
+            print(f"{s.name}: {s.matter_2}")
+
+def rank_matter_3(self):
+        self.students.sort(key=lambda s: s.matter_3, reverse=True)
+        for s in self.students:
+            print(f"{s.name}: {s.matter_3}")
 
 if __name__ == '__main__':
     school_class = SchoolClass()
     school_class.add_student(Student('J', 10, 12, 13))
     school_class.add_student(Student('A', 8, 2, 17))
     school_class.add_student(Student('V', 9, 14, 14))
+    
     school_class.rank_matter_1()
+    school_class.rank_matter_2()
+    school_class.rank_matter_3()
