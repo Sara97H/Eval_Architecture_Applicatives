326bd25d308eb01bf02231561343b2661bf4be1e
Sara97H <sarahaddad.sh97@gmail.com>
2026-03-25 14:00:00 +0100

    Q6 : Iterator et Iterable matter 1

--- school.py
+++ school.py
@@ -1,38 +1,53 @@
+from collections.abc import Iterable, Iterator
 class Student:
     def __init__(self, name, matter_1, matter_2, matter_3):
         self.name = name
         self.matter_1 = matter_1
         self.matter_2 = matter_2
         self.matter_3 = matter_3
-
-class SchoolClass:
-    def __init__(self):
-        self.students = []
-
-    def add_student(self, student):
+class Matter1Iterator(Iterator):
+    def __init__(self, students):
+        self.students = sorted(students, key=lambda s: s.matter_1, reverse=True)
+        self.index = 0
+    def __next__(self):
+        if self.index < len(self.students):
+            res = self.students[self.index]
+            self.index += 1
+            return res
+        raise StopIteration
+    
+class SchoolClass(Iterable):
+     def __init__(self):
+         self.students = []
+     def add_student(self, student):
         self.students.append(student)
+     def __iter__(self):
+        return Matter1Iterator(self.students)
 
-def rank_matter_1(self):
-        self.students.sort(key=lambda s: s.matter_1, reverse=True)
-        for s in self.students:
-            print(f"{s.name}: {s.matter_1}")
+# def rank_matter_1(self):
+#         self.students.sort(key=lambda s: s.matter_1, reverse=True)
+#         for s in self.students:
+#             print(f"{s.name}: {s.matter_1}")
         
-def rank_matter_2(self):
-        self.students.sort(key=lambda s: s.matter_2, reverse=True)
-        for s in self.students:
-            print(f"{s.name}: {s.matter_2}")
+# def rank_matter_2(self):
+#         self.students.sort(key=lambda s: s.matter_2, reverse=True)
+#         for s in self.students:
+#             print(f"{s.name}: {s.matter_2}")
 
-def rank_matter_3(self):
-        self.students.sort(key=lambda s: s.matter_3, reverse=True)
-        for s in self.students:
-            print(f"{s.name}: {s.matter_3}")
+# def rank_matter_3(self):
+#         self.students.sort(key=lambda s: s.matter_3, reverse=True)
+#         for s in self.students:
+#             print(f"{s.name}: {s.matter_3}")
 
 if __name__ == '__main__':
     school_class = SchoolClass()
     school_class.add_student(Student('J', 10, 12, 13))
     school_class.add_student(Student('A', 8, 2, 17))
     school_class.add_student(Student('V', 9, 14, 14))
-    
-    school_class.rank_matter_1()
-    school_class.rank_matter_2()
-    school_class.rank_matter_3()
+
+    # school_class.rank_matter_1()
+    # school_class.rank_matter_2()
+    # school_class.rank_matter_3()
+
+    for s in school_class:
+        print(f"{s.name}: {s.matter_1}")
